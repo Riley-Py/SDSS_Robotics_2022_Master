6bb9a888518ccab0b7748abbc78623b1cc5085bd
InterestingError0 <63620497+InterestingError0@users.noreply.github.com>
2023-01-21 16:01:46 -0500

    Remove setting pneumatic to true when ButtonA is pressed

--- src/main.cpp
+++ src/main.cpp
@@ -194,9 +194,6 @@ void opcontrol() {
 		if(controller.get_digital(DIGITAL_X)) {
 			pneumatic.set_value(true);
 		}
-		if(controller.get_digital(DIGITAL_A)) {
-			pneumatic.set_value(true);
-		}
 
 		controller.print(1, 1, "%i", flywheel.is_over_temp());
 
